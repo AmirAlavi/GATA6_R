b12219b54fbc8aaff7385431ecc5d8655e7e08db
Amir Alavi <s.amir.alavi@gmail.com>
2020-02-20 15:01:18 -0500

    More memory efficient data conversion script

--- nowotschin_2_seurat.R
+++ nowotschin_2_seurat.R
@@ -9,14 +9,14 @@ saveRDS(counts, file = "nowotschin_data/sc_endoderm_all_cells_counts.RDS")
 saveRDS(meta, file = "nowotschin_data/sc_endoderm_all_cells_metadata.RDS")
 
 # Get E6.5
-e6.5.counts <- counts[meta$Timepoint == "E6.5",]
-setDF(e6.5.counts)
-tmp <- e6.5.counts[, -1]
-rownames(tmp) <- e6.5.counts[, 1]
-e6.5.counts <- tmp
-e6.5.meta <- meta[meta$Timepoint == "E6.5",]
-nowotschin_e6.5 <- CreateSeuratObject(counts = t(e6.5.counts), project = "NowotschinE6.5", assay = "RNA", meta.data = e6.5.meta)
-nowotschin_e6.5.filtered <- CreateSeuratObject(counts = t(e6.5.counts), project = "NowotschinE6.5", assay = "RNA", meta.data = e6.5.meta, min.cells = 5, min.features = 200)
+counts <- counts[meta$Timepoint == "E6.5",]
+setDF(counts)
+tmp <- counts[, -1]
+rownames(tmp) <- counts[, 1]
+counts <- tmp
+meta <- meta[meta$Timepoint == "E6.5",]
+nowotschin_e6.5 <- CreateSeuratObject(counts = t(counts), project = "NowotschinE6.5", assay = "RNA", meta.data = meta)
+nowotschin_e6.5.filtered <- CreateSeuratObject(counts = t(counts), project = "NowotschinE6.5", assay = "RNA", meta.data = meta, min.cells = 5, min.features = 200)
 
 saveRDS(nowotschin_e6.5, file = "nowotschin_data/nowotschin_E6.5_seurat_object.RDS")
 saveRDS(nowotschin_e6.5.filtered, file = "nowotschin_data/nowotschin_E6.5_filtered_seurat_object.RDS")
